12de8736e6c980bb74333ca5f507bc64a12a3365
Andr?s Jank? <andras.janko@gmail.com>
2015-03-31 17:20:44 +0000

    remove alpha tag

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    (BuildTool().PackageId("WebSharper.WebAudio", "3.0-alpha")
+    (BuildTool().PackageId("WebSharper.WebAudio", "3.0")
     |> fun bt -> bt.WithFramework(bt.Framework.Net40))
         .References(fun r -> [r.NuGet("WebSharper.WebRTC").Reference()])
 
