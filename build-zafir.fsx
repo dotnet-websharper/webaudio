ed555169e75a1f4dc37b16b8c885595dd438311a
Istvan Gansperger <gansperger@gmail.com>
2015-12-07 14:58:37 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,42 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.WebAudio")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+
+let main =
+    (bt.Zafir.Extension("WebSharper.WebAudio")
+    |> FSharpConfig.BaseDir.Custom "WebAudio")
+        .SourcesFromProject("WebAudio.fsproj")
+        .References(fun r ->
+            [
+                r.NuGet("WebSharper.WebRTC").ForceFoundVersion().Reference()
+            ])
+
+(*let test =
+    (bt.WebSharper.BundleWebsite("IntelliFactory.WebSharper.WebAudio.Tests")
+    |> FSharpConfig.BaseDir.Custom "Tests")
+        .SourcesFromProject("Tests.fsproj")
+        .References(fun r -> [r.Project main])*)
+
+bt.Solution [
+    main
+    //test
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.WebAudio"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.webaudio"
+                Description = "WebSharper Extensions for WebAudio"
+                Authors = ["IntelliFactory"]
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+
+]
+|> bt.Dispatch
