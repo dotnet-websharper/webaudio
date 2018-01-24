2e2e994e9a148dc33fb5b320e07fdeebf709c91c
Loic Denuziere <loic@denuziere.net>
2018-01-24 13:44:32 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,40 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.WebAudio")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let main =
-    bt.WebSharper.Extension("IntelliFactory.WebSharper.WebAudio", directory = "WebAudio")
-        .SourcesFromProject("WebAudio.fsproj")
-        .References(fun r ->
-            [
-                r.NuGet("WebSharper.WebRTC").Version("(,4.0)").ForceFoundVersion().Reference()
-            ])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-(*let test =
-    bt.WebSharper.BundleWebsite("IntelliFactory.WebSharper.WebAudio.Tests", directory = "Tests")
-        .SourcesFromProject("Tests.fsproj")
-        .References(fun r -> [r.Project main])*)
-
-bt.Solution [
-    main
-    //test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.WebAudio"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://bitbucket.org/intellifactory/websharper.webaudio"
-                Description = "WebSharper Extensions for WebAudio"
-                Authors = ["IntelliFactory"]
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
